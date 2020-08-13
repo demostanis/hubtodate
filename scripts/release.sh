f3bf2d381e1994de09ca76288a7d707be213cc1e
demostanis <demostanis@protonmail.com>
2020-08-13 20:27:30 +0200

    Release script signs with GPG

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/env sh
 
 # Script used to generate release
-# TODO: Sign archives with GPG
 
 mkdir -p dist
 tar caf dist/hubtodate.tar.gz lib/ rules/ hubtodate META6.json
+gpg --sign dist/hubtodate.tar.gz
+mv dist/hubtodate.tar.gz.gpg dist/hubtodate.tar.gz
 sha256sum dist/hubtodate.tar.gz > dist/sha256sums.txt
 echo "done"
