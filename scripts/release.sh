aaa26f6f4651b0c7b9da57aed494e0ec20f65a3d
demostanis <demostanis@protonmail.com>
2020-08-13 22:07:49 +0200

    No GPG signing due to problems with gzip

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,8 +4,4 @@
 
 mkdir -p dist
 tar caf dist/hubtodate.tar.gz lib/ rules/ hubtodate META6.json
-cd dist
-gpg --sign hubtodate.tar.gz
-mv hubtodate.tar.gz.gpg hubtodate.tar.gz
-sha256sum hubtodate.tar.gz > sha256sums.txt
-echo "done"
+cd dist && sha256sum hubtodate.tar.gz > sha256sums.txt
